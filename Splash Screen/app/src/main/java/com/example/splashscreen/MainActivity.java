094ed40a470ce479d852fddbc30e8ba83e523d2f
David <davidarrozaqi085@gmail.com>
2023-03-01 21:49:23 +0700

    Splash Screen

---
+++ Splash Screen/app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        setContentView();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
